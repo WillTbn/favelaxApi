ec969aa7921cf5e04ad8e20dcd84c896652924f6
JorgeNunes <jlbnunes@live.com>
2023-06-22 00:29:12 -0300

    feature:configure laravel passport

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => env('USER_ADMIN_EMAIL'),
+            'password' => bcrypt(env('USER_ADMIN_PASSWORD'))
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::get('/401', [AuthController::class, 'unauthorized'])->name('unauthorized');
+
+Route::group(['prefix' => 'auth'], function(){
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
 });
+Route::controller(AuthController::class)->group(function(){
+    Route::group(['prefix'=>'user'],
+        function($router){
+            Route::get('details', 'getUserDetail');
+            Route::post('logout', 'logout');
+
+        });
+})->middleware('auth:api');
+
+// Route::middleware('auth:api')->group(function () {
+//     Route::post('/logout', 'AuthController@logout');
+//     Route::get('/user', function (Request $request) {
+//         return $request->user();
+//     });
+// });
